2bbe2914f103bae9ee904febcdb7fdc9d1ce8ae8
Fahmi Derbali <fahmiderbali@gmail.com>
2021-04-21 22:34:19 -0400

    Une autre route pour connaitre l'organisateur d'une evenement

--- server/routes/index.js
+++ server/routes/index.js
@@ -210,6 +210,23 @@ router.get("/activite/:id/membres", async (req, res, next) => {
 	}
 });
 
+router.get("/activite/:id/organisateur", async (req, res, next) => {
+	try {
+		let whereClause = new WhereClause('?? = ?', ['id_activite', req.params.id]);
+		let result_id_memb = await Organise.find(whereClause);
+		let array_id_membre = [];
+		for (element of result_id_memb) {
+			array_id_membre.push(element.id_membre);
+		}
+		liste_id_membre = "(" + array_id_membre.join(",") + ")";
+		let whereClausemembres = new WhereClause('id IN ' + liste_id_membre, []);
+		let membres = await Membre.find(whereClausemembres);
+		res.json(membres[0]);
+	} catch (e) {
+		res.json({ 'success': false });
+	}
+});
+
 router.post("/membre/inscrire", async (req, res, next) => {
 	try {
 		let participe = new Participe();
